eb0a210f2ed66ccd0f67d03c47a349c40418ed83
LUTHFI BAEHAQI <117705394+baehaqi312@users.noreply.github.com>
2025-09-20 05:15:15 +0700

    Memperbaiki

---
+++ src/layouts/auth/AuthSimpleLayout.vue
@@ -0,0 +1,54 @@
+<script setup>
+import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+defineProps({
+    title: {
+        type: String,
+        required: false
+    },
+    description: {
+        type: String,
+        required: false
+    }
+});
+
+const goHome = () => {
+    router.push('/');
+};
+</script>
+
+<template>
+    <div
+        class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10"
+    >
+        <div class="w-full max-w-sm">
+            <div class="flex flex-col gap-8">
+                <div class="flex flex-col items-center gap-4">
+                    <button
+                        @click="goHome"
+                        class="flex flex-col items-center gap-2 font-medium cursor-pointer"
+                    >
+                        <div
+                            class="mb-1 flex h-9 w-9 items-center justify-center rounded-md"
+                        >
+                            <AppLogoIcon
+                                class="size-9 fill-current text-[var(--foreground)] dark:text-white"
+                            />
+                        </div>
+                        <span class="sr-only">{{ title }}</span>
+                    </button>
+                    <div class="space-y-2 text-center">
+                        <h1 class="text-xl font-medium">{{ title }}</h1>
+                        <p class="text-center text-sm text-muted-foreground">
+                            {{ description }}
+                        </p>
+                    </div>
+                </div>
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
