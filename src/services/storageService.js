43de65de3a9d7f365a79e5dbcd59ca0809a385f2
LUTHFI BAEHAQI <117705394+baehaqi312@users.noreply.github.com>
2025-09-20 17:05:18 +0700

    Update Pengguna

--- src/services/storageService.js
+++ src/services/storageService.js
@@ -6,10 +6,10 @@ const LOGGED_IN_USER_KEY = 'miftahul_huda_loggedInUser';
 export const initStorage = () => {
     if (!localStorage.getItem(USERS_KEY)) {
         localStorage.setItem(USERS_KEY, JSON.stringify([
-            { id: 1, name: 'Admin', email: 'admin@admin.com', password: 'password', role: 'admin' },
-            { id: 2, name: 'User Biasa', email: 'user@user.com', password: 'password', role: 'user' },
-            { id: 3, name: 'John Doe', email: 'john@example.com', password: 'password', role: 'user' },
-            { id: 4, name: 'Jane Smith', email: 'jane@example.com', password: 'password', role: 'admin' }
+            { id: 1, name: 'Admin', email: 'luthfi@gmail.com', password: '123456', role: 'admin' },
+            { id: 2, name: 'User Biasa', email: 'agung123@gmail.com', password: '123456', role: 'user' },
+            { id: 3, name: 'John Doe', email: 'autay123@gmail.com', password: '123456', role: 'admin' },
+            { id: 4, name: 'Jane Smith', email: 'adam123@gmail.com', password: '123456', role: 'user' }
         ]));
     }
     if (!localStorage.getItem(ITEMS_KEY)) {
